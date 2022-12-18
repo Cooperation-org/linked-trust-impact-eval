7ff716daae9c9284fe982025a57110bae3070cd0
ArnobChowdhury <chowdhury.sami.al@gmail.com>
2022-12-18 16:50:59 +0600

    Mutate query to composedb to make claim

--- task/pages/review.js
+++ task/pages/review.js
@@ -3,8 +3,40 @@ import styles from "../styles/Home.module.css";
 import { Inter } from "@next/font/google";
 import Layout from "../components/Layout";
 import { useViewerConnection } from "@self.id/framework";
+import { getCompose } from "../compose";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const CREATE_CLAIM = `
+  mutation (
+    $claim: String!
+    $by: String!
+    $credit: Int!
+    $round: String!
+  ){
+    createClaim(
+      input: {
+        content: {
+          claim: $claim
+          by: $by
+          credit: $credit
+          round: $round
+        }
+      }
+    ){
+      document {
+        id
+        claim
+        by
+        credit
+        signed_by {
+          id
+        }
+        round
+      }
+    }
+  }
+`;
 
 export default function Review() {
   const [tasks, setTasks] = useState([]);
@@ -20,13 +52,12 @@ export default function Review() {
 
   let tasksComponent;
   if (tasks.length > 0) {
-    tasksComponent = tasks.map((task) => {
-      const by = task.by;
+    tasksComponent = tasks.map((taskInDB) => {
+      const { id, task, by, credit, round } = taskInDB;
       const acc = by.slice(0, 4) + "..." + by.slice(34);
-      console.log(acc);
       return (
         <div
-          key={task.id}
+          key={id}
           className={inter.className}
           style={{
             padding: "20px",
@@ -38,13 +69,13 @@ export default function Review() {
           }}
         >
           <div style={{ fontWeight: "300" }}>Task:</div>
-          <div style={{ fontWeight: "600" }}>{task.task}</div>
+          <div style={{ fontWeight: "600" }}>{task}</div>
           <div style={{ fontWeight: "300" }}>Claimant:</div>
           <div style={{ fontWeight: "600" }}>{acc}</div>
           <div style={{ fontWeight: "300" }}>Credit:</div>
-          <div style={{ fontWeight: "600" }}>{task.credit}</div>
+          <div style={{ fontWeight: "600" }}>{credit}</div>
           <div style={{ fontWeight: "300" }}>Round:</div>
-          <div style={{ fontWeight: "600" }}>{task.round}</div>
+          <div style={{ fontWeight: "600" }}>{round}</div>
           <div
             style={{
               paddingTop: "20px",
@@ -54,8 +85,25 @@ export default function Review() {
           >
             <button
               className={styles.btn}
-              onClick={() => {
-                console.log("Approving");
+              onClick={async () => {
+                const compose = await getCompose(connection.selfID.did);
+                const variables = {
+                  claim: task,
+                  by,
+                  credit: Number(credit),
+                  round,
+                };
+                const composeDBResult = await compose.executeQuery(
+                  CREATE_CLAIM,
+                  variables
+                );
+
+                if (!composeDBResult.errors) {
+                  setTasks(() => {
+                    return tasks.filter((task) => task.id !== id);
+                  });
+                }
+
                 // Make the claim in composedb
               }}
             >
