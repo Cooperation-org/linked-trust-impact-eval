97bc34eb170a4cd016165c296306bcf3fcf344f8
stan36 <chris@dorg.tech>
2023-02-06 18:51:57 -0800

    updated bacalahau server side to work with single IPFS CID

--- server/bacalhau.sh
+++ server/bacalhau.sh
@@ -1,38 +1,12 @@
 #!/bin/bash
-declare -a array=("$@")
-# get length of an array
-arraylength=${#array[@]}
+declare -a cid=("$@")
+
 
 STR1="bacalhau wasm run ./bacalhau_compute.wasm "
 STR=""
-MAX=0
-end=${arraylength}
-# use for loop to read all values and indexes
-for (( i=0; i<${arraylength}; i++ ));
-do
-  if [ $i -eq 0 ]
-  then
-    STR1="${STR1}"
-  else
-  STRI="-v ${array[$i]}:CID$i "
-  if [ $i -eq 1 ]
-  then
-    STR="${STR1}${STRI}"
-  else
-    STR="${STR}${STRI}"
-  fi
-fi
-done
 
-for (( i=0; i<${arraylength}; i++ ));
-do
-  if [ $i -eq 0 ]
-  then
-    STR="${STR} ${array[$i]}"
-  else
-    STR="${STR} CID$i"
-fi
-done
+STR="${STR1} -v ${cid}:CID CID | tee jobID.txt"
+
 
 echo "${STR}"
  $STR > jobID.txt
@@ -40,3 +14,4 @@ echo "${STR}"
  bacalhau get $(grep "Job ID:" jobID.txt | cut -f2 -d:) --output-dir wasm_results
 
 # bacalhau wasm run ./bacalhau_compute.wasm -v QmSCMeGh17KJWA9VHPftWqA1xdWM7McVtt3azxH3aenp1X:CID1 -v QmZhCRWWv8x6zJKhUKwnDNzerWoUaAjyeD4aSY8tGz6qE4:CID2 -v QmT3sHvzDBV53WqY2Z2Gt5U9zFPZYHKHMSKghbbkxt3zsM:CID3 1000 CID1 CID2 CID3 | tee jobID.txt
+# bacalhau wasm run ./bacalhau_compute.wasm -v QmTFcE6qwbpN5YozkR7u8yztZGYYjAztuxeF3VQMpAJwh1:CID1   CID1  | tee jobID.txt
