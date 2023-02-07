b1d7eefa27c873e82564e87d4cc4811616dec377
Arnob Chowdhury <chowdhury.sami.al@gmail.com>
2023-02-07 21:02:18 +0600

    Pin claims to web3.storage

--- composedb/app/db.py
+++ composedb/app/db.py
@@ -1,8 +1,7 @@
 import sqlite3
 import os
 
-# db_path2 = os.getenv("DB_PATH")
-db_path = "./mydb"
+db_path = os.getenv("DB_PATH")
 
 conn = sqlite3.connect(db_path, check_same_thread=False)
 cursor = conn.cursor()
---
+++ task/pages/api/bacalhau.js
@@ -0,0 +1,30 @@
+import path from "path";
+import { promises as fs, existsSync, mkdirSync } from "fs";
+import { Web3Storage, getFilesFromPath } from "web3.storage";
+
+const token = process.env.WEB3STORAGE_API_KEY;
+const storage = new Web3Storage({ token });
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { claims, limit } = req.body;
+    const fileContent = JSON.stringify({ limit, claims });
+
+    const targetDirectory = path.join(process.cwd(), "claims");
+    if (!existsSync(targetDirectory)) {
+      mkdirSync(targetDirectory);
+    }
+    const pathToFile = targetDirectory + "/claims.json";
+    let cid;
+    try {
+      await fs.writeFile(pathToFile, fileContent);
+      const file = await getFilesFromPath(pathToFile);
+      cid = await storage.put(file);
+    } catch (err) {
+      return res.status(500).json({ message: "Something went wrong!" });
+    }
+
+    return res.status(200).json({ cid });
+  }
+  res.status(500).json({ message: "Something went wrong!" });
+}
